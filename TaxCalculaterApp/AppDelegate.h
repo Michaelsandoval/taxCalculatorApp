655af35ffb76b06b73fd5ffcc10b3eba962b463c
Michael Sandoval <michaelsandoval@michaels-macbook-pro.local>
2016-01-10 19:13:12 -0600

    Initial Commit

---
+++ TaxCalculaterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculaterApp
+//
+//  Created by Michael Sandoval on 1/10/16.
+//  Copyright © 2016 Michael Sandoval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalculaterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculaterApp
+//
+//  Created by Michael Sandoval on 1/10/16.
+//  Copyright © 2016 Michael Sandoval. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
